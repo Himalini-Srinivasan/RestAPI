26b3c4c0049ec2ca93d8ce714154e8f1b7b4d247
Himalini-Srinivasan <150216477+Himalini-Srinivasan@users.noreply.github.com>
2024-03-04 16:15:45 +0530

    Delete DAY11/d11onetoonebi directory

--- DAY11/d11onetoonebi/src/main/java/com/example/d11onetoonebi/repo/PersonRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.d11onetoonebi.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.d11onetoonebi.model.Person;
-
-public interface PersonRepo extends JpaRepository<Person,Long>{
-    
-}
