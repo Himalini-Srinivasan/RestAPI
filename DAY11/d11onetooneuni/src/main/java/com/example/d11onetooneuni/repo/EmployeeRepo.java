9e0c82dfa6db404bd5c396080e422d45db2354b7
Himalini-Srinivasan <150216477+Himalini-Srinivasan@users.noreply.github.com>
2024-03-04 16:16:12 +0530

    Delete DAY11/d11onetooneuni directory

--- DAY11/d11onetooneuni/src/main/java/com/example/d11onetooneuni/repo/EmployeeRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.d11onetooneuni.repo;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.d11onetooneuni.model.Employee;
-
-public interface EmployeeRepo extends JpaRepository<Employee,Long> {
-    
-}
